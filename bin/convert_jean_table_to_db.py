d9fbaa05497f71b40dda3fb3f790235c13798f3d
Justin Fear <justin.m.fear@gmail.com>
2017-04-10 09:08:31 -0400

    updates jean conversion table.

--- bin/convert_jean_table_to_db.py
+++ bin/convert_jean_table_to_db.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
-"""Take metadata information from jean and adds to database."""
+"""Take metadata information from Jean's summary and adds to database."""
+
 import pandas as pd
 from pymongo import MongoClient
 import numpy as np
+import re
+import logging
+
+logger = logging.getLogger()
 
 def parse_meig_dat(fname):
+    """Parse the Mieg file specifying my own headers.
+
+    The Mieg headers were a little awkward, so I decided to clean these up a
+    little. I tried to keep them similar so that they would be easily
+    translated back.
+    """
     header = pd.MultiIndex.from_tuples([
         ('magic', 'id'),
         ('magic', 'srr'),
@@ -81,20 +92,64 @@ def parse_meig_dat(fname):
     return df.apply(set_runs, axis=1)
 
 
+def split_srr(df):
+    """Splits ('magic', 'srr') column into separate rows.
+
+    I am thinking it will be more useful to split SRRs up into their own
+    record. When added to the database I am using set logic based on values so
+    I would get a list of SRRs that is easy to pull out.
+    """
+    rows = []
+    for i, sr in df.iterrows():
+        srr = sr[('magic', 'srr')]
+        if ';' in srr:
+            srrs = [x.strip() for x in sr[('magic', 'srr')].split('\\;')]
+            for run in srrs:
+                sr[('magic', 'srr')] = run
+                rows.append(sr)
+        else:
+            rows.append(sr)
+    return pd.concat(rows, axis=1, ignore_index=True).T
+
+
 def get_level(x, dat):
+    """Quick helper to pull out records from a dict based that is keyed with a
+    tuple.
+
+    The keys are of the form {(index1, index2): value}. I want to pull out
+    everthing corresponding to index1 and return [{index2: value},].
+
+    """
+    pattern = re.compile(r'[ \\\(\)\?]+')
     vals = []
-    for key, value in dat.items():
-        if key[0] == x:
-            if not isinstance(value, str) and np.isnan(value):
-                continue
+    for keys, value in dat.items():
+        if keys[0] == x:
+            if not isinstance(value, str) and np.isnan(value): continue
+
+            if x != 'magic':
+                key = re.sub(pattern, '_', keys[1]).strip('_').lower()
+                value = re.sub(pattern, '_', value).strip('_').lower()
+            else:
+                key = keys[1]
+                value = value
+
+            if value == 'none': continue
+
             vals.append({
-                'name': key[1],
+                'name': key,
                 'value': value
             })
+
     return vals
 
-def undo(biometa, record):
-    """Undo the changes."""
+
+def undo(biometa, df):
+    """Undo the changes.
+
+    Since I am playing with the database want a way to easily undo the changes
+    if I need to.
+
+    """
     for record in df.to_dict('record'):
         pk = record[('magic', 'biosample')]
         biometa.find_one_and_update(
@@ -110,17 +165,10 @@ def undo(biometa, record):
             }
         )
 
-if __name__ == '__main__':
-    # Import Jean's summary
-    fname = '../../ncbi_remap/data/jean/RunsDroso_4annot_9549_libraries.txt'
-    df = parse_meig_dat(fname)
 
-    # Connect to database
-    client = MongoClient(host='localhost', port=27022)
-    db = client['test']
-    biometa = db['biometa']
-
+def add_to_db(biometa, df):
     # Add jean's annotation summary to database
+    logger.info('Adding records to database')
     for record in df.to_dict('record'):
         pk = record[('magic', 'biosample')]
         biometa.find_one_and_update(
@@ -134,4 +182,25 @@ if __name__ == '__main__':
                     'nlm': {'$each': get_level('nlm', record)},
                 }
             }
-            )
+        )
+
+
+if __name__ == '__main__':
+    # Import Jean's summary
+    logger.info('Importing data')
+    fname = '../../ncbi_remap/data/jean/RunsDroso_4annot_9549_libraries.txt'
+    df = parse_meig_dat(fname)
+
+    # split up srrs in Jean's table
+    dfSplit = split_srr(df)
+
+    # Connect to database
+    logger.info('Connecting to database')
+    client = MongoClient(host='localhost', port=27022)
+    db = client['test']
+    biometa = db['biometa']
+
+    # Add to database
+    add_to_db(biometa, dfSplit)
+#     undo(biometa, dfSplit)
+
