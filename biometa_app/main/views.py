1f24aaa1218f550a672e6dad799b9b08d9d5e19a
Justin Fear <justin.m.fear@gmail.com>
2017-06-28 16:50:52 -0400

    Complete re-work of view.
    This is a complete re-work of the two main page view.

--- biometa_app/main/views.py
+++ biometa_app/main/views.py
@@ -1,28 +1,86 @@
 from flask import render_template, redirect, request, flash, url_for, current_app, jsonify
+from flask_login import current_user
 from . import main_bp
-from .forms import PerPageForm
+from .forms import PerPageForm, QuerySampleAttribute, AttributeForm
 from ..models import Biometa
+from biometalib.models import CleanedAttributes
 
 
 @main_bp.route("/", methods=["GET", "POST"])
 def home():
+    """Home page which contains a table with a list of samples."""
+
     # Get any request information from the URL
     page = int(request.args.get('page', 1))
-    per_pages = int(request.args.get('per_pages', 20))
+    payload = {
+        'per_pages': int(request.args.get('per_pages', 20)),
+        'srr': request.args.get('srr', ''),
+        'srx': request.args.get('srx', ''),
+        'query': request.args.get('query', ''),
+    }
 
-    # Get the form
-    form = PerPageForm(request.form)
+    # Pull in search form.
+    query_form = QuerySampleAttribute()
+    if (request.method == 'POST') and query_form.search.data and query_form.validate_on_submit():
+        payload['srr'] = query_form['SRR'].data
+        payload['srx'] = query_form['SRX'].data
+        payload['query'] = query_form['Attributes'].data
+        return redirect(url_for("main.home", **payload))
 
-    if request.method == 'POST':
-        per_pages = int(form.per_pages.data)
+    if payload['srr']:
+        cursor = Biometa.objects(experiments__runs__icontains=payload['srr'])
+    elif payload['srx']:
+        cursor = Biometa.objects(experiments__srx=payload['srx'])
+    elif payload['query']:
+        cursor = Biometa.objects(sample_attributes__value__icontains=payload['query'])
     else:
-        form.per_pages.default = per_pages
+        cursor = Biometa.objects()
+
+    # If there is only one item then send to the item page.
+    if cursor.count() == 1:
+        bio = cursor[0]
+        return redirect(url_for("main.sample", sample=bio.biosample))
+
+    # Pull in pagination form
+    form = PerPageForm()
+
+    if (request.method == 'POST') and form.submit.data:
+        payload['per_pages'] = int(form.per_pages.data)
+        return redirect(url_for("main.home", **payload))
+    else:
+        form.per_pages.default = payload['per_pages']
         form.process()
 
-    paginated_table = Biometa.objects.order_by('biosample').paginate(page=page, per_page=per_pages)
-    return render_template('index.html', paginated_table=paginated_table, form=form)
+    paginated_table = cursor.order_by('biosample').paginate(page=page, per_page=payload['per_pages'])
+    return render_template('index.html', paginated_table=paginated_table, form=form, query=query_form, current_user=current_user, payload=payload)
 
 @main_bp.route("/<sample>", methods=["GET", "POST"])
 def sample(sample):
-    sample = Biometa.objects.get_or_404(pk=sample)
-    return render_template('sample.html', sample=sample)
+    sample_data = Biometa.objects.get_or_404(pk=sample)
+    form = AttributeForm()
+
+    if (request.method == 'POST') and form.validate_on_submit():
+        _data = {k: v for k, v in form.data.items() if (v != '') & (k != 'csrf_token')& (k != 'submit')}
+        Biometa.objects(pk=sample).update_one(set__user_annotation={current_user.username: CleanedAttributes(**_data)})
+        flash("Updated record.", "success")
+        return render_template('sample.html', sample=sample_data, form=form)
+    try:
+        _data = sample_data.user_annotation[current_user.username].to_mongo().to_dict()
+    except KeyError:
+        _data = {}
+
+    attrs = [x['name'] for x in sample_data.sample_attributes]
+    for i in form:
+        key = i.id
+        if _data.get(key, ''):
+            value = _data[key]
+        elif key == 'sample_title':
+            value = sample_data.sample_title
+        elif key in attrs:
+            value = [x['value'] for x in sample_data.sample_attributes if x['name'] == key][0]
+        else:
+            value = ''
+        form[key].default = value
+    form.process()
+
+    return render_template('sample.html', sample=sample_data, form=form)
