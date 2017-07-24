ae597fa97494428515098f2939f99c43fc226950
Justin Fear <justin.m.fear@gmail.com>
2017-07-24 11:58:09 -0400

    Rename AttributeSelector form.
    My form and data model had the same name, so append "Form" onto the form
    name to make "AttributeSelectorForm".

--- app/attribute/forms.py
+++ app/attribute/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField, IntegerField, FloatField, BooleanField, StringField
 from wtforms import validators
 
-class AttributeSelector(FlaskForm):
+class AttributeSelectorForm(FlaskForm):
     KeepButton = SubmitField('Keep', description="Click to keep current attribute type.")
     IgnoreButton = SubmitField('Ignore', description="Click to ignore current attribute type.")
     RenameButton = SubmitField('Rename', description="Click to Rename current attribute type.")
