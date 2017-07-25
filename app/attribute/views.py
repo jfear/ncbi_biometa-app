32a03f0b7f81ce41680f00b650c51e743eec7c94
Justin Fear <justin.m.fear@gmail.com>
2017-07-25 13:52:21 -0400

    Attribute selector now has suggestions added.

--- app/attribute/views.py
+++ app/attribute/views.py
@@ -15,7 +15,13 @@ from .models import AttributeSelector
 
 
 def get_all_attrs():
-    """Grab all attribute types from the Biometa database."""
+    """Grab all attribute types from the Biometa database.
+
+    Returns
+    -------
+    list
+        sorted list containing all attribute types from the database.
+    """
     cursor = Biometa.objects.aggregate(*[
         {'$unwind': '$sample_attributes'},
         {
@@ -30,6 +36,12 @@ def get_user():
     """Look up the current users AttributeSelection document.
 
     If this document is empty then initialize with flybase_example.
+
+    Returns
+    -------
+    list of .models.AttributeValue
+        Returns a list of AttributeValue objects. Using a list to help maintain
+        order of attributes.
     """
     _user = AttributeSelector.objects(user=current_user.username).first()
     if _user is None:
@@ -42,7 +54,13 @@ def get_user():
 
 
 def get_user_attrs():
-    """Get dictionary of attribute types mapped to user selected attribute types."""
+    """Get list of attributes already categorized by the current user.
+
+    Returns
+    -------
+    list
+        A list of attributes already categorized by the current user.
+    """
     userAttrs = get_user()
     _attr = []
     for doc in userAttrs.attributes:
@@ -52,7 +70,13 @@ def get_user_attrs():
 
 
 def filter_attribute_list():
-    """Filter out attributes that are already in user attributes."""
+    """Filter out attributes already categorized by the current user.
+
+    Returns
+    -------
+    list
+        Reversed list of attributes not already categorized by the current user.
+    """
     all_attrs = get_all_attrs()
     user_attrs = get_user_attrs()
     _attrs = []
@@ -63,7 +87,10 @@ def filter_attribute_list():
 
 
 def add_attr(key, value):
-    """Add key:value pairs to user selected attributes."""
+    """Add key:value pairs to user selected attributes.
+
+    Helper function to add attributes to the current user's document.
+    """
     # The AttributeSelector document for the current user.
     userDoc = get_user()
 
@@ -87,7 +114,13 @@ def add_attr(key, value):
 def get_index(name, docs, field='name'):
     """Get index from list of dicts.
 
-    Given a list of dicts, figure out the index that contains the value.
+    Given a list of dicts, figure out the index that contains the value in the
+    corresponding field.
+
+    Returns
+    -------
+    int or None
+        Retruns the index a value belongs to, or None
     """
     for i, doc in enumerate(docs):
         if doc[field] == name:
@@ -95,20 +128,67 @@ def get_index(name, docs, field='name'):
     return None
 
 
+def get_examples(currAttr):
+    """Get a list of current values.
+
+    Given an attribute, pull out all of the values in that attribute column
+    from the database. Also calculate basic information about the attribute
+    use.
+
+    Returns
+    -------
+    int
+        Number of BioSamples that have this attribute type
+    int
+        Number of BioProjects that have this attribute type
+    list
+        List of values that correspond to this attribute type
+    """
+    num_samples = len(list(Biometa.objects.aggregate(*[
+        {'$unwind': '$sample_attributes'},
+        {'$match': {'sample_attributes.name': currAttr}},
+    ])))
+
+    # Count the number of projects that used attribute
+    num_projects = len(list(Biometa.objects.aggregate(*[
+        {'$unwind': '$sample_attributes'},
+        {'$match': {'sample_attributes.name': currAttr}},
+        {'$group': {'_id': '$bioproject'}},
+    ])))
+
+    # Get a list of all values from the attribute
+    values = Biometa.objects.aggregate(*[
+        {'$unwind': '$sample_attributes'},
+        {'$match': {'sample_attributes.name': currAttr}},
+        {
+            '$project': {'_id': '$sample_attributes.value'}
+        }
+    ])
+
+    return num_samples, num_projects, list(set([x['_id'] for x in values]))
+
+
 @attribute_bp.route("/attribute", methods=["GET", "POST"])
 @login_required
 def attribute_selector():
+    """Run the attribute selector.
+
+    Originally designed as a CLI, attribute selector is a way to look at all
+    attribute types and decide if you want to keep, ignore, or rename. This
+    page will iterate over all attribute types in the database and get user
+    input about how to handle the type.
+    """
     form = AttributeSelectorForm()
 
-    # On GET pull session data
     if request.method == 'GET':
         # Get a list of attribute types
-        pass
-    session['attrList'] = session.get('attrList', filter_attribute_list())
-    try:
-        session['currAttr'] = session.get('currAttr', session['attrList'].pop())
-    except IndexError:
-        return redirect(url_for('.attribute_map'))
+        attrList = filter_attribute_list()
+
+        # Get current attribute or send to summary page
+        try:
+            session['currAttr'] = attrList.pop()
+        except IndexError:
+            return redirect(url_for('.attribute_map'))
 
     if request.method == 'POST':
         currAttr = session['currAttr']
@@ -122,19 +202,39 @@ def attribute_selector():
             # Add the attribute to the renamed value
             add_attr(form.Rename.data, currAttr)
 
-        try:
-            session['currAttr'] = session['attrList'].pop()
-            return redirect(url_for('.attribute_selector'))
-        except IndexError:
-            return redirect(url_for('.attribute_map'))
+        return redirect(url_for('.attribute_selector'))
 
-    return render_template('attribute.html', form=form, current_user=current_user, attribute=session['currAttr'])
+
+    # Get information about the current attribute
+    num_samples, num_projects, examples = get_examples(session['currAttr'])
+
+    # Get information about the current user's list of attribute types
+    user_attr = [x['name'] for x in get_user().attributes]
+
+    return render_template('attribute.html', form=form, current_user=current_user,
+                           attribute=session['currAttr'], num_samples=num_samples,
+                           num_projects=num_projects, examples=examples, user_attr=user_attr)
 
 
 @attribute_bp.route("/attributeMap", methods=["GET",])
 def attribute_map():
+    """Summary page of attribute type classification.
+
+    It may be useful to look at all of the attributes and see how other users
+    have classified them. This is a simple summary page where usernames are
+    placed next to the attribute type. Top level attributes are the user's
+    naming designation, while sub attributes are to be renamed to the top level
+    attribute type.
+
+    All of this code is data munging. The database is organized by user at the
+    top level, I need attribute at the top level and then a list of users that
+    have that attribute.
+    """
     attrDict = json.loads(AttributeSelector.objects.to_json())
+
+    # Make a new list to hold the re-organizedata.
     attrs = []
+    # Iterate over each user
     for user in attrDict:
         username = user['_id']
         for _attr in user['attributes']:
