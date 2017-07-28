883442512c1d757ae22cc5a2a62d91298fa722d5
Justin Fear <justin.m.fear@gmail.com>
2017-07-28 09:46:11 -0400

    working version of datatables with basic search.

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,6 @@
+import os
+from collections import OrderedDict
+from itertools import chain
 import json
 from flask import render_template, redirect, request, flash, url_for, current_app
 from flask_login import current_user
@@ -8,57 +11,22 @@ from biometalib.models import CleanedAttributes
 from biometalib.models import Biometa as Bm
 
 
-@main_bp.route("/", methods=["GET", "POST"])
-def home():
-    """Home page which contains a table with a list of samples."""
+columnMapping = OrderedDict([
+    ('BioSample', '$_id'),
+    ('BioProject', '$bioproject'),
+    ('SRA Study', '$srs'),
+    ('SRA Project', '$srp'),
+    ('SRA Experiments', '$experiments.srx'),
+    ('SRA Runs', '$experiments.runs'),
+])
 
-    # Get any request information from the URL
-    page = int(request.args.get('page', 1))
-    payload = {
-        'per_pages': int(request.args.get('per_pages', 20)),
-        'srr': request.args.get('srr', ''),
-        'srx': request.args.get('srx', ''),
-        'srp': request.args.get('srp', ''),
-        'query': request.args.get('query', ''),
-    }
 
-    # Pull in search form.
-    query_form = QuerySampleAttribute()
-    if (request.method == 'POST') and query_form.search.data and query_form.validate_on_submit():
-        payload['srr'] = query_form['SRR'].data
-        payload['srx'] = query_form['SRX'].data
-        payload['srp'] = query_form['SRP'].data
-        payload['query'] = query_form['Attributes'].data
-        return redirect(url_for("main.home", **payload))
+@main_bp.route("/", methods=["GET"])
+@main_bp.route("/<osearch>")
+def home(osearch=''):
+    """Playing with datatables."""
+    return render_template('index.html', columns=columnMapping.keys(), osearch=osearch)
 
-    if payload['srr']:
-        cursor = Biometa.objects(experiments__runs__icontains=payload['srr'])
-    elif payload['srx']:
-        cursor = Biometa.objects(experiments__srx=payload['srx'])
-    elif payload['srp']:
-        cursor = Biometa.objects(srp=payload['srp'])
-    elif payload['query']:
-        cursor = Biometa.objects(sample_attributes__value__icontains=payload['query'])
-    else:
-        cursor = Biometa.objects()
-
-    # If there is only one item then send to the item page.
-    if cursor.count() == 1:
-        bio = cursor[0]
-        return redirect(url_for("main.sample", sample=bio.biosample))
-
-    # Pull in pagination form
-    form = PerPageForm()
-
-    if (request.method == 'POST') and form.submit.data:
-        payload['per_pages'] = int(form.per_pages.data)
-        return redirect(url_for("main.home", **payload))
-    else:
-        form.per_pages.default = payload['per_pages']
-        form.process()
-
-    paginated_table = cursor.order_by('biosample').paginate(page=page, per_page=payload['per_pages'])
-    return render_template('index.html', paginated_table=paginated_table, form=form, query=query_form, current_user=current_user, payload=payload)
 
 @main_bp.route("/<sample>", methods=["GET", "POST"])
 def sample(sample):
@@ -66,7 +34,7 @@ def sample(sample):
     form = AttributeForm()
 
     if (request.method == 'POST') and form.validate_on_submit():
-        _data = {k: v for k, v in form.data.items() if (v != '') & (k != 'csrf_token')& (k != 'submit')}
+        _data = {k: v for k, v in form.data.items() if (v != '') & (k != 'csrf_token') & (k != 'submit')}
         Biometa.objects(pk=sample).update_one(set__user_annotation={current_user.username: CleanedAttributes(**_data)})
         flash("Updated record.", "success")
         return render_template('sample.html', sample=sample_data, form=form)
@@ -91,31 +59,59 @@ def sample(sample):
 
     return render_template('sample.html', sample=sample_data, form=form)
 
-@main_bp.route("/dt", methods=["GET", "POST"])
-def dt():
-    """Playing with datatables."""
-    return render_template('index2.html')
+
+def join_list(ll):
+    """Join a list of values.
+
+    Joins a list of values with a |, and adds a line break every 4 items. Also
+    flattens 2d lists.
+    """
+    # flatten if 2d list
+    if isinstance(ll[0], list):
+        flat = list(set(chain(*ll)))
+    else:
+        flat = ll
+
+    # Join adding returns
+    out = ''
+    for i, value in enumerate(flat):
+        if i == 0:
+            out = str(value)
+        elif i % 4 == 0:
+            out += '|\n' + str(value)
+        else:
+            out += '|' + str(value)
+    return out
+
 
 @main_bp.route("/_dt", methods=["GET", "POST"])
 def get_server_data():
-    """Playing with datatables."""
+    """Build the datatable."""
     pipeline = [{
             '$project': {
                 '_id': 0,
-                'BioSample': '$_id',
-                'BioProject': '$bioproject',
-                'SRA Study': '$srs',
-                'SRA Project': '$srp',
-                'SRA Experiments': {
-                    '$reduce': {
-                        'input': '$experiments.srx',
-                        'initialValue': '',
-                        'in': { '$concat': ['$$value', '|', '$$this'] }
-                    }
-                },
+                **columnMapping
             },
         },
         ]
 
-    return json.dumps({'data': list(Bm.objects.aggregate(*pipeline))})
+    payload = []
+    for record in Bm.objects.aggregate(*pipeline):
+        # clean missing columns
+        for key in columnMapping.keys():
+            if key not in record:
+                record[key] = ''
 
+        # Add link to BioSample
+        bs = record['BioSample']
+        url = url_for("main.sample", sample=bs)
+        record['BioSample'] = '<a href="{}">{}</a>'.format(url, bs)
+
+        # Concatenat SRX and SRRs
+        record['SRA Experiments'] = join_list(record['SRA Experiments'])
+        record['SRA Runs'] = join_list(record['SRA Runs'])
+
+        # Save record
+        payload.append(record)
+
+    return json.dumps({'data': payload})
